49b2748e49ff2af5a818d98619fe3d1894dde104
morningstar <developermorningstar@gmail.com>
2018-11-15 10:25:52 +0530

    Adding image from gallery added. 15/11/2018

--- app/src/main/java/com/morningstar/finland/ui/MainActivity.java
+++ app/src/main/java/com/morningstar/finland/ui/MainActivity.java
@@ -8,21 +8,39 @@
 
 package com.morningstar.finland.ui;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.morningstar.finland.R;
 import com.morningstar.finland.utility.DrawerUtils;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private Toolbar toolbar;
     private FloatingActionButton fromGallery, fromDrive;
 
+    private static final int REQUEST_CODE_GALLERY = 1;
+    private static final int EXTERNAL_STORAGE_PERMISSION_CODE = 69;
+
+    private Uri uri;
+    private String file_path;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +57,11 @@ public class MainActivity extends AppCompatActivity {
         fromGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                if (!(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)) {
+                    requestPermission();
+                } else {
+                    chooseImageFromGallery();
+                }
             }
         });
 
@@ -49,5 +71,72 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void chooseImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Select Image"), REQUEST_CODE_GALLERY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (resultCode == RESULT_OK) {
+                if (requestCode == REQUEST_CODE_GALLERY) {
+                    if (data != null) {
+                        uri = data.getData();
+                        file_path = uri != null ? uri.getPath() : null;
+                        Log.i(TAG, "Uri: " + uri);
+                        Log.i(TAG, "File Path: " + file_path);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.i(TAG, "Exception caught while parsing data");
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission Required")
+                    .setMessage("External Storage Permission is needed to get images from your gallery")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_PERMISSION_CODE);
+                            dialog.dismiss();
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create()
+                    .show();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == EXTERNAL_STORAGE_PERMISSION_CODE) {
+            if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Toast.makeText(this, "This feature requires external storage permission", Toast.LENGTH_SHORT).show();
+            } else
+                chooseImageFromGallery();
+        }
     }
 }
